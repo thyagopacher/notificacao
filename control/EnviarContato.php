3cb9fd9b648382f83946c4d81d141c61e1e0d889
Thyago  Henrique Pacher <thyago.pacher@gmail.com>
2021-03-04 15:33:44 -0300

    Update EnviarContato.php

--- control/EnviarContato.php
+++ control/EnviarContato.php
@@ -46,8 +46,8 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'appabaraujo@gmail.com';                 // SMTP username
-$mail->Password = 'Brasil1602*';                           // SMTP password
+$mail->Username = '5555@gmail.com';                 // SMTP username
+$mail->Password = '****';                           // SMTP password
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 $mail->setFrom($pessoap["email"], $pessoap["nome"]);
